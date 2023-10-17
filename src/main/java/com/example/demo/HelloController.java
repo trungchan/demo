77a831f4f75aeeebe7e636ab81eb61a8ea275d94
trungchan <trungtq95@gmail.com>
2023-10-17 21:29:48 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class HelloController {
+
+    @GetMapping("hello")
+    public ResponseEntity<?> hello(){
+
+        return new ResponseEntity<>("hello", HttpStatus.OK);
+    }
+}
